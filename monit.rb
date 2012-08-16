14a720a5e8eb19c57f8c5fef98bed16e6e839f75
Railsgirl42 <railsgirl42@rocketmail.com>
2012-08-16 20:12:38 +0100

    Fixed autostart monit

--- monit.rb
+++ monit.rb
@@ -7,9 +7,9 @@ dep 'monit.managed'
 
 dep "autostart monit" do
   requires 'monit.managed'
-  met? { !"/etc/default/monit".to_fancypath.grep(/^[^#]*startup=0/) && File.exists?("/etc/init/monit.conf") }
+  met? { !"/etc/default/monit".to_fancypath.grep(/^[^#]*START=yes/) && File.exists?("/etc/init/monit.conf") }
   meet {
-    shell("sudo sed -i'' -e 's/^startup=0$/startup=1/' '/etc/default/monit'")
+    shell("sudo sed -i'' -e 's/^START=no$/START=yes/' '/etc/default/monit'")
     # remove existing monit startscripts
     # sudo("update-rc.d -f monit remove")
     # unless(File.exists?("/etc/init/monit.conf"))
