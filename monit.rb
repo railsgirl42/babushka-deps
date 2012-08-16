33c35a399220c2ae1812e77e729af7171c6a552a
Railsgirl42 <railsgirl42@rocketmail.com>
2012-08-16 19:55:53 +0100

    Fixed use of Fancypath in autostart monit dep

--- monit.rb
+++ monit.rb
@@ -7,9 +7,9 @@ dep 'monit.managed'
 
 dep "autostart monit" do
   requires 'monit.managed'
-  met? { !FancyPath.grep(/^[^#]*startup=0/, "/etc/default/monit") && File.exists?("/etc/init/monit.conf") }
+  met? { !Fancypath.grep(/^[^#]*startup=0/, "/etc/default/monit") && File.exists?("/etc/init/monit.conf") }
   meet {
-    shell("sed -i'' -e 's/^startup=0$/startup=1/' '/etc/default/monit'")
+    shell("sudo sed -i'' -e 's/^startup=0$/startup=1/' '/etc/default/monit'")
     # remove existing monit startscripts
     # sudo("update-rc.d -f monit remove")
     # unless(File.exists?("/etc/init/monit.conf"))
